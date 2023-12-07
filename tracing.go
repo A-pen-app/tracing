3b1c2a20353b9ac840ec3ceaca852d1c4042f68c
Jonas Chen <bbhands.chen@gmail.com>
2023-12-07 12:26:10 +0800

    feat: support custom timeout

--- tracing.go
+++ tracing.go
@@ -1,7 +1,9 @@
+// Package tracing provides handy helper functions for measuring API performance and debugging.
 package tracing
 
 import (
 	"context"
+	"time"
 
 	texporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace"
 	"go.opentelemetry.io/contrib/detectors/gcp"
@@ -18,28 +20,38 @@ var projectID string
 var serviceName string
 var deploymentEnvironment string
 var tracerName string
+var timeout int64
 
+// Config provides customization of tracing
 type Config struct {
 	ProjectID             string `json:"project_id" yaml:"project_id"`
 	TracerName            string `json:"tracer_name" yaml:"tracer_name"`
 	ServiceName           string `json:"service_name" yaml:"service_name"`
 	DeploymentEnvironment string `json:"deployment_environment" yaml:"deployment_environment"`
+	TimeoutInSeconds      int64  `json:"timeout_in_seconds" yaml:"timeout_in_seconds"`
 }
 
+// Start creates a new span for a given name
 func Start(ctx context.Context, name string) trace.Span {
 	_, span := tp.Tracer(tracerName).Start(ctx, name)
 	return span
 }
 
+// Initialize setup for tracing
 func Initialize(ctx context.Context, c *Config) error {
+	options := []texporter.Option{}
 	if c != nil {
 		projectID = c.ProjectID
 		serviceName = c.ServiceName
 		deploymentEnvironment = c.DeploymentEnvironment
 		tracerName = c.TracerName
+		timeout = c.TimeoutInSeconds
 	}
-
-	exporter, err := texporter.New(texporter.WithProjectID(projectID))
+	options = append(options, texporter.WithProjectID(projectID))
+	if timeout > 0 {
+		options = append(options, texporter.WithTimeout(time.Duration(timeout)*time.Second))
+	}
+	exporter, err := texporter.New(options...)
 	if err != nil {
 		return err
 	}
@@ -65,6 +77,7 @@ func Initialize(ctx context.Context, c *Config) error {
 	return nil
 }
 
+// Finalize cleanup tracing
 func Finalize(ctx context.Context) {
 	if tp != nil {
 		tp.Shutdown(ctx)
